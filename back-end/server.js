66c59b88c81c1bad7ed7192a7820c72464753ac5
Idowu Joseph <idowujo672@gmail.com>
2025-06-11 17:44:38 +0100

    4th integration

--- back-end/server.js
+++ back-end/server.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: ['http://localhost:5173', 
   'http://localhost:5174',
   'https://elite-admin-three.vercel.app',
-  'https://elite-realestate.vercel.app/'
+  'https://elite-realestate.vercel.app'
 ],// or wherever your frontend is hosted
   credentials: true
 }));
